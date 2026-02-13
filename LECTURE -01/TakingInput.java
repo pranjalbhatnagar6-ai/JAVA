acf68924acf9690391fff7d07032f46c0e285bc8
pranjalbhatnagar6-ai <pranjalbhatnagar6@gmail.com>
2026-02-13 18:16:35 +0530

    Learning - taking String input

--- LECTURE -01/TakingInput.java
+++ LECTURE -01/TakingInput.java
@@ -1,11 +1,17 @@
 import java.util.Scanner;
-
 public class TakingInput{
     public static void main(String[] args){
+        // for integer
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the no."); //print - Ussi line me print karne ke liye
-        int num_1 = sc.nextInt(); //int dtype - for integer value.
-        System.out.println("no. is : " + num_1); //println - next line me print karne ke liye.
+        // System.out.print("Enter the no."); //print - Ussi line me print karne ke liye
+        // int num_1 = sc.nextInt(); //int dtype - for integer value.
+        // System.out.println("no. is : " + num_1); //println - next line me print karne ke liye.
         
+        // for String
+        System.out.print("Enter your name : ");
+        // String name = sc.next(); //next - only print the word before space.
+        String name = sc.nextLine(); //next - only print the word before space.
+        System.out.print("The name is.... "+ name);
+
     }
 }
