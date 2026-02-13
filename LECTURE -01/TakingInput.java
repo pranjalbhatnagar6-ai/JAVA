dd1a1ef2254ad3c795f2c95f47d3edb2e6876f54
pranjalbhatnagar6-ai <pranjalbhatnagar6@gmail.com>
2026-02-13 17:52:59 +0530

    Learning - How to take input?

---
+++ LECTURE -01/TakingInput.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TakingInput{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no."); //print - Ussi line me print karne ke liye
+        int num_1 = sc.nextInt(); //int dtype - for integer value.
+        System.out.println("no. is : " + num_1); //println - next line me print karne ke liye.
+        
+    }
+}
