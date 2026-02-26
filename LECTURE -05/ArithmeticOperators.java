0692de365029bc592fd4fbca64297d49f4e86f45
pranjalbhatnagar6-ai <pranjalbhatnagar6@gmail.com>
2026-02-26 21:16:45 +0530

    Learning Operators

---
+++ LECTURE -05/ArithmeticOperators.java
@@ -0,0 +1,17 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int p = 20, q = 10;
+        // System.out.println(p + q);
+        // System.out.println(p - q);
+        // System.out.println(p * q);
+        // System.out.println(p / q);
+        // System.out.println(p % q);
+        // Relational_Operator
+        System.out.println(p == q);
+        System.out.println(p != q);
+        System.out.println(p <= q);
+        System.out.println(p >= q);
+        System.out.println(p > q);
+        System.out.println(p < q);
+    }
+}
